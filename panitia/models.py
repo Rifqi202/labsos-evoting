22a816ccda16984b48ea822147d58c2459e8018e
hanifmisbah <hanifmisbah97@gmail.com>
2022-01-30 15:11:28 +0700

    pull

--- panitia/models.py
+++ panitia/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
-
 class daftarkandidat(models.Model):
     namakandidat = models.TextField(max_length=100, default='')
     nomerurut = models.IntegerField(default='')
